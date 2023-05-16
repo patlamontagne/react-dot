0b4bb076f7591b0ff0eaad314c83116c714cf555
Patrick Lamontagne <16765115+patlamontagne@users.noreply.github.com>
2023-05-16 10:04:14 -0400

    Update ErrorComponent.jsx

--- src/ErrorComponent.jsx
+++ src/ErrorComponent.jsx
@@ -1,21 +1,21 @@
+import 'react';
+
 export function ErrorComponent(props) {
     return (
         <div
             style={{
                 padding: 16,
-                background: "rgb(150, 80, 80)",
-                color: "white",
-            }}
-        >
+                background: 'rgb(150, 80, 80)',
+                color: 'white',
+            }}>
             <div
                 style={{
-                    fontSize: "1rem",
-                    fontFamily: "Arial, sans-serif",
-                    fontWeight: "bold",
+                    fontSize: '1rem',
+                    fontFamily: 'Arial, sans-serif',
+                    fontWeight: 'bold',
                     marginBottom: 8,
-                }}
-            >
-                React-dot could not render the component.
+                }}>
+                React-dot could not render the {props.component} component.
             </div>
             <code>{props.message}</code>
         </div>
